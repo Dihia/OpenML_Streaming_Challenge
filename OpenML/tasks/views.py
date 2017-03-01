481fe43bf985b69dd349c59e5875960c34497067
dihia <dihia.boulegane@gmail.com>
2017-03-01 14:44:54 +0100

    get first task

--- OpenML/tasks/views.py
+++ OpenML/tasks/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from openml import tasks,datasets
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    task = tasks.get_task(9)
+    data = datasets.get_dataset(244)
+    #r=requests.get("http://www.openml.org/api/v1/task/9?key_api = a2ac595c56b398c8db7a4be791db9083")
+    return HttpResponse(str(task.task_id))
