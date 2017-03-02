960785375c67e102ef5ee8eb384ec587bc940a63
emixam150 <maxime@buron.coffee>
2017-03-02 19:25:17 +0100

    openml scrapping

--- OpenML/OpenML/tasks/views.py
+++ OpenML/OpenML/tasks/views.py
@@ -8,7 +8,34 @@ variables = {
     "pageName" : "Tasks"
 }
 
+task_example1 = {
+    "name": "Supervised Data Stream Classification on blabal",
+    "id": 123,
+    "nb_runs": 0,
+    "nb_likes": 1,
+    "nb_downloads": 2,
+    "nb_reachs": 3,
+    "nb_impacts": 4
+}
+
+task_example = {
+    "name": "Supervised Data Stream Classification on molecular-biology_promoters",
+    "id": 23,
+    "nb_runs": 0,
+    "nb_likes": 1,
+    "nb_downloads": 2,
+    "nb_reachs": 3,
+    "nb_impacts": 4
+}
+
 def index(request):
-    task_list = Contact.objects.all()
-    variables['task_list'] = task_list
+
+    task_list = [task_example,task_example1]
+    variables['tasks'] = task_list
+
     return render(request, 'tasks/index.html', variables)
+
+def show(request):
+
+    variables['task'] = task_example
+    return render(request,'tasks/show.html',variables)
--- OpenML/OpenML/urls.py
+++ OpenML/OpenML/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^tasks/', include('OpenML.tasks.urls')),
+    url(r'^t/', include('OpenML.tasks.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'', include('OpenML.home.urls'))
 ]
