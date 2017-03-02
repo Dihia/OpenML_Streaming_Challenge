fea22f03c47c4c017d5517b0a8a7a668d28634a8
emixam150 <maxime@buron.coffee>
2017-03-02 14:37:27 +0100

    reorginzation of folders

--- OpenML/OpenML/settings.py
+++ OpenML/OpenML/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OpenML.tasks'
 ]
 
 MIDDLEWARE = [
@@ -54,9 +55,7 @@ ROOT_URLCONF = 'OpenML.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 os.path.join(BASE_DIR, 'tasks/templates')
-                 ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
