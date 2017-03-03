28198ca24ba64f8eb4b38bbe09607c58e398848e
Dihia <dihia.boulegane@gmail.com>
2017-03-03 09:47:16 +0100

    test

--- OpenML/tasks/views.py
+++ OpenML/tasks/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render
 
 from openml import tasks
 
-task_list = [tasks.get_task(9)]
+#task_list = [tasks.get_task(2056)]
+task_list_id= tasks.list_tasks(task_type_id=9)
+task_list=[]
+
+for id in task_list_id:
+    try:
+        task_list.append(tasks.get_task(id))
+    except:
+        pass
+
 
 
 
 def get(request):
-    return render(request, 'tasks.html', {'tasks': task_list})
+    return render(request, 'tasks.html', {'tasks': task_list_id})
 
